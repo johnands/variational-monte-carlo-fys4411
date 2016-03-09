Fixed sign error in drift force. Automatic optimization and production run. Added report

PythonScripts/blocking.py
@@ -46,38 +46,66 @@ def mean(blockEnergies):
 def main():
 
     # extract data and convert to array
-    energies = np.array(extract('energy.dat'))
-
+    energies = np.array(extract('energyInteracting0.7.dat'))
+   
+    """
     # block parameters
     numberOfEnergies = len(energies)
     blockSizeStepLength = 50 #(maximumBlockSize - minimumBlockSize) / numberOfBlockSizes
     minimumBlockSize = 1
-    maximumBlockSize = 10000
+    maximumBlockSize = len(energies) / 10
     numberOfBlockSizes = (maximumBlockSize - minimumBlockSize) / blockSizeStepLength
-	
+    """
 
+    
+    # block parameters
+    numberOfEnergies = len(energies)
+    maxNumberOfBlocks = numberOfEnergies/3
+    minNumberOfBlocks = 100
+    numberOfBlocksStep = 100
+    numberOfBlockNumbers = (maxNumberOfBlocks - minNumberOfBlocks) / numberOfBlocksStep
+    
+    """
     print 'No of energies = ', numberOfEnergies
     print 'Min size = '      , minimumBlockSize
     print 'Max size = '      , maximumBlockSize
     print 'Step length = '   , blockSizeStepLength
     print 'Number of block sizes = ', numberOfBlockSizes
+    """
 
+    print 'No of energies = ', numberOfEnergies
+    print 'Min number = ', minNumberOfBlocks
+    print 'Max number = ', maxNumberOfBlocks
+    print 'Step length = ', numberOfBlocksStep
+    print 'Number of block numbers = ', numberOfBlockNumbers
+
+    """
     # loop over block sizes
     meanOfGivenBlockSize 		= np.zeros(numberOfBlockSizes)
     varianceOfGivenBlockSize 	= np.zeros(numberOfBlockSizes)
     allBlockSizes 				= np.zeros(numberOfBlockSizes)
     allNumberOfBlocks 			= np.zeros(numberOfBlockSizes)
-    for i in xrange(numberOfBlockSizes):
-        blockSize = minimumBlockSize + i*blockSizeStepLength
-        numberOfBlocks = numberOfEnergies / blockSize
-        allBlockSizes[i] = blockSize
+    """
+
+    # loop over number of blocks
+    meanOfGivenBlockSize 		= np.zeros(numberOfBlockNumbers)
+    varianceOfGivenBlockSize 	= np.zeros(numberOfBlockNumbers)
+    allBlockSizes 				= np.zeros(numberOfBlockNumbers)
+    allNumberOfBlocks 			= np.zeros(numberOfBlockNumbers)
+
+    for i in xrange(numberOfBlockNumbers):
+        #blockSize = minimumBlockSize + i*blockSizeStepLength
+        numberOfBlocks = minNumberOfBlocks + i*numberOfBlocksStep
+        #numberOfBlocks = numberOfEnergies / blockSize
+        blockSize = numberOfEnergies / numberOfBlocks
         allNumberOfBlocks[i] = numberOfBlocks
+        allBlockSizes[i] = blockSize
         finalMean, finalVariance = blocking(energies, numberOfBlocks, blockSize)
         meanOfGivenBlockSize[i] = finalMean
         varianceOfGivenBlockSize[i] = finalVariance
 
         # show progress
-        sys.stdout.write("\r{0}".format((float(i)/numberOfBlockSizes)*100))
+        sys.stdout.write("\r{0}".format((float(i)/numberOfBlockNumbers)*100))
         sys.stdout.flush()
 
     return meanOfGivenBlockSize, varianceOfGivenBlockSize, allBlockSizes, allNumberOfBlocks
@@ -86,9 +114,10 @@ def main():
 
 # main
 meanOfGivenBlockSize, varianceOfGivenBlockSize, allBlockSizes, allNumberOfBlocks = main()
-plt.plot(allBlockSizes, varianceOfGivenBlockSize)
+plt.plot(allNumberOfBlocks, np.sqrt(varianceOfGivenBlockSize))
+plt.xlabel('Number of blocks')
+plt.ylabel('Standard deviation')
 plt.show()
-print
 
 
 