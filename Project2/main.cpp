b87b8890cbb50fdf16a154179a8c1eac2ca065ae
John-Anders Stende <jastende@gmail.com>
2016-04-11 18:28:21 +0200

    New directory

---
+++ Project2/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include "system.h"
+#include "particle.h"
+#include "WaveFunctions/wavefunction.h"
+#include "WaveFunctions/simplegaussian.h"
+#include "WaveFunctions/interactinggaussian.h"
+#include "Hamiltonians/hamiltonian.h"
+#include "Hamiltonians/harmonicoscillator.h"
+#include "Hamiltonians/harmonicoscillatorinteracting.h"
+#include "InitialStates/initialstate.h"
+#include "InitialStates/randomuniform.h"
+#include "steepestdescent.h"
+#include "Math/random.h"
+
+using namespace std;
+
+int main() {
+
+    int numberOfDimensions  = 2;
+    int numberOfParticles   = 10;
+    int numberOfSteps       = (int) 1e6;
+    double omega            = 1.0;          // oscillator frequency
+    double alpha            = 0.5;    // variational parameter 1
+    double beta             = 2.82843;      // variational parameter 2
+    double stepLength       = 1.5;          // metropolis step length
+    double equilibration    = 0.1;          // amount of the total steps used for equilibration
+    double timeStep         = 0.005;        // importance sampling
+    double a                = 0.0043;       // hard sphere radius
+    double gamma            = 2.82843;      // trap potential strength z-direction
+
+    bool useNumerical       = false;        // compute kinetic energy numerically
+    bool useImportanceSampling = false;
+    bool writeEnergiesToFile = false;
+    bool writePositionsToFile = false;
+
+    System* system = new System();
+    //system->setHamiltonian              (new HarmonicOscillator(system, omega, useNumerical));
+    system->setHamiltonian              (new HarmonicOscillatorInteracting(system, omega, a, gamma, useNumerical));
+    //system->setWaveFunction             (new SimpleGaussian(system, alpha));
+    system->setWaveFunction             (new InteractingGaussian(system, alpha, beta, a));
+    system->setInitialState             (new RandomUniform(system, numberOfDimensions, numberOfParticles));
+    system->setEquilibrationFraction    (equilibration);
+    system->setStepLength               (stepLength);
+    system->setTimeStep                 (timeStep);
+    system->runMetropolisSteps          (numberOfSteps, useImportanceSampling, writeEnergiesToFile, writePositionsToFile);
+
+    // optimize alpha
+    /*double initialAlpha = 0.7;
+    double stepLengthOptimize = 0.01;
+    SteepestDescent* sd  = new SteepestDescent(system, stepLengthOptimize);
+    sd->optimize(initialAlpha);*/
+
+    return 0;
+}
